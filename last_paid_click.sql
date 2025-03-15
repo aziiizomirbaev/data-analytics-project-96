c0df8e5a3627adf3cdf8a242a7b3a4557320c816
Aziz 'Isaac' Omirbaev <azizomirbaevahalo@gmail.com>
2025-03-15 19:12:48 +0500

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,14 +1,3 @@
-/*
-visitor_id — уникальный человек на сайте
-visit_date — время визита
-utm_source / utm_medium / utm_campaign — метки c учетом модели атрибуции
-lead_id — идентификатор лида, если пользователь сконвертился в лид после(во время) визита, NULL — если пользователь не оставил лид
-created_at — время создания лида, NULL — если пользователь не оставил лид
-amount — сумма лида (в деньгах), NULL — если пользователь не оставил лид
-closing_reason — причина закрытия, NULL — если пользователь не оставил лид
-status_id — код причины закрытия, NULL — если пользователь не оставил лид
-*/
-
 SELECT
     s.visitor_id,
     s.visit_date,
@@ -29,4 +18,3 @@ ORDER BY
     utm_source ASC,
     utm_medium ASC,
     utm_campaign ASC
-LIMIT 10;
