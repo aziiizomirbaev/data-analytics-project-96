e48f0631e021a3db1f2f1f9876a287673e926c42
Aziz 'Isaac' Omirbaev <azizomirbaevahalo@gmail.com>
2025-03-16 19:54:01 +0500

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,20 +1,34 @@
-SELECT
-    s.visitor_id,
-    s.visit_date,
-    s.source AS utm_source,
-    s.medium AS utm_medium,
-    s.campaign AS utm_campaign,
-    l.lead_id,
-    l.created_at,
-    l.amount,
-    l.closing_reason,
-    l.status_id
-FROM sessions AS s
-LEFT JOIN leads AS l ON s.visitor_id = l.visitor_id
-WHERE s.medium != 'organic'
-ORDER BY
-    l.amount DESC NULLS LAST,
-    s.visit_date ASC,
-    utm_source ASC,
-    utm_medium ASC,
-    utm_campaign ASC
+WITH tab AS (
+	SELECT 
+		s.visitor_id,
+		s.visit_date, 
+		s.source AS utm_source, 
+		s.medium AS utm_medium, 
+		s.campaign AS utm_campaign, 
+		l.lead_id, 
+		l.amount, 
+		l.closing_reason, 
+		l.status_id, 
+		ROW_NUMBER() OVER(PARTITION BY s.visitor_id ORDER BY s.visit_date DESC) AS last_click 
+	FROM sessions AS s 
+	LEFT JOIN leads AS l ON s.visitor_id = l.visitor_id 
+	WHERE s.medium IN ('cpc', 'cpm', 'cpa', 'cpp', 'tg', 'youtube', 'social')
+) 
+SELECT 
+	visitor_id,
+	visit_date, 
+	utm_source, 
+	utm_medium, 
+	utm_campaign, 
+	lead_id, 
+	amount, 
+	closing_reason, 
+	status_id
+FROM tab 
+WHERE last_click = 1 
+ORDER BY 
+	amount DESC NULLS LAST, 
+	visit_date ASC, 
+	utm_source ASC, 
+	utm_medium ASC, 
+	utm_campaign ASC
