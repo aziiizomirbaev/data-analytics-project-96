6de8bcec6b107ad5c1852a8a50eb0d87ede0881c
Aziz 'Isaac' Omirbaev <azizomirbaevahalo@gmail.com>
2025-03-23 01:02:16 +0500

    Add files via upload

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -1,87 +1,70 @@
 WITH tab AS (
-	SELECT visitor_id, MAX(visit_date) AS visit_date
-	FROM sessions
+	SELECT visitor_id, MAX(visit_date) AS last_visit
+	FROM sessions 
 	WHERE medium != 'organic'
-	GROUP BY 1
-), lpc AS (
+	GROUP BY 1 
+), last_paid AS (
 	SELECT 
-		s.visitor_id,
-		s.visit_date, 
-		s.source AS utm_source, 
-		s.medium AS utm_medium, 
-		s.campaign AS utm_campaign, 
-		l.lead_id, 
-		l.created_at, 
-		l.amount, 
-		l.closing_reason, 
+		s.visitor_id, 
+	    s.visit_date::date,
+	    s.source AS utm_source,
+	    s.medium AS utm_medium,
+	    s.campaign AS utm_campaign,
+	    l.lead_id,
+	    l.created_at,
+	    l.amount, 
 		l.status_id
 	FROM sessions AS s 
-	INNER JOIN tab AS t ON s.visitor_id = t.visitor_id AND s.visit_date = t.visit_date 
-	LEFT JOIN leads AS l ON s.visitor_id = l.visitor_id AND s.visit_date <= l.created_at
-), last_paid_click_aggregate AS (
-	SELECT 
-		visit_date::date, 
-		utm_source, 
-		utm_medium, 
-		utm_campaign, 
-		COUNT(visitor_id) AS visitors_count, 
-		COUNT(lead_id) AS leads_count,
-		SUM(CASE WHEN status_id = 142 THEN 1 ELSE 0 END) AS purchases_count, 
-		SUM(amount) AS revenue
-	FROM lpc 
-	GROUP BY 
-		visit_date::date, 
-		utm_source, 
-		utm_medium, 
-		utm_campaign 
-), ads_cost AS (
+	INNER JOIN tab AS t 
+	ON s.visit_date = t.last_visit AND s.visitor_id = t.visitor_id
+	LEFT JOIN leads AS l 
+	ON s.visit_date <= l.created_at AND s.visitor_id = l.visitor_id
+), ads AS (
 	SELECT 
 		campaign_date::date, 
 		utm_source, 
-		utm_medium, 
+		utm_medium,
 		utm_campaign,
-		SUM(daily_spent) AS total_cost 	
+		SUM(daily_spent) AS spent
 	FROM vk_ads 
-	GROUP BY 
-		campaign_date::date, 
-		utm_source, 
-		utm_medium, 
-		utm_campaign
-	UNION 
+	GROUP BY 1, 2, 3, 4
+	UNION ALL 
 	SELECT 
 		campaign_date::date, 
 		utm_source, 
-		utm_medium, 
-		utm_campaign,
-		SUM(daily_spent) AS total_cost 	
+		utm_medium,
+		utm_campaign, 
+		SUM(daily_spent) AS spent
 	FROM ya_ads 
-	GROUP BY 
-		campaign_date::date, 
-		utm_source, 
-		utm_medium, 
-		utm_campaign
+	GROUP BY 1, 2, 3, 4
 )
 
 SELECT 
-	lpca.visit_date, 
-	lpca.visitors_count, 
-	lpca.utm_source, 
-	lpca.utm_medium, 
-	lpca.utm_campaign, 
-	ad.total_cost, 
-	lpca.leads_count, 
-	lpca.purchases_count, 
-	lpca.revenue
-FROM last_paid_click_aggregate AS lpca
-LEFT JOIN ads_cost AS ad ON 
-	lpca.visit_date::date = ad.campaign_date::date
-	AND	lpca.utm_source = ad.utm_source
-	AND lpca.utm_medium = ad.utm_medium 
-	AND lpca.utm_campaign = ad.utm_campaign
+	lp.visit_date::date, 
+	lp.utm_source, 
+	lp.utm_medium, 
+	lp.utm_campaign, 
+	COUNT(DISTINCT lp.visitor_id) AS visitors_count, 
+	a.spent AS total_cost, 
+	COUNT(DISTINCT lp.lead_id) AS leads_count, 
+	COUNT(DISTINCT lp.lead_id) FILTER(WHERE lp.status_id = 142) AS purchases_count, 
+	SUM(lp.amount) AS revenue 
+FROM last_paid AS lp 
+LEFT JOIN ads AS a 
+	ON lp.visit_date = a.campaign_date 
+	AND lp.utm_source = a.utm_source 
+	AND lp.utm_medium = a.utm_medium
+	AND lp.utm_campaign = a.utm_campaign
+GROUP BY 
+	lp.visit_date, 
+	lp.utm_source, 
+	lp.utm_medium, 
+	lp.utm_campaign, 
+	a.spent
 ORDER BY 
-	lpca.revenue DESC NULLS LAST, 
-	lpca.visit_date ASC, 
-	lpca.visitors_count DESC, 
+	revenue DESC NULLS LAST, 
+	lp.visit_date ASC, 
+	visitors_count DESC, 
 	utm_source ASC, 
 	utm_medium ASC, 
 	utm_campaign ASC
