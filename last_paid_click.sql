88939f4553f8d8f68b59f658c773921b608e02ac
Aziz 'Isaac' Omirbaev <azizomirbaevahalo@gmail.com>
2025-03-26 18:25:57 +0500

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,28 +1,39 @@
 WITH tab AS (
-	SELECT visitor_id, MAX(visit_date) AS last_visit
-	FROM sessions 
-	WHERE medium != 'organic'
-	GROUP BY 1 
-) 
+    SELECT
+        visitor_id,
+        MAX(visit_date) AS last_visit
+    FROM sessions
+    WHERE medium != 'organic'
+    GROUP BY visitor_id  
+)
 
-SELECT 
-	s.visitor_id,
-    TO_CHAR(s.visit_date, 'YYYY-MM-DD HH24:MI:SS.MS') AS visit_date,
-    s.source AS utm_source,
-    s.medium AS utm_medium,
-    s.campaign AS utm_campaign,
-    l.lead_id,
-    TO_CHAR(l.created_at, 'YYYY-MM-DD HH24:MI:SS.MS') AS created_at,
-    l.amount,
-    l.closing_reason,
-    l.status_id
-FROM sessions AS s 
-INNER JOIN tab AS t ON s.visit_date = t.last_visit AND s.visitor_id = t.visitor_id
-LEFT JOIN leads AS l ON s.visitor_id = l.visitor_id AND s.visit_date <= l.created_at
-ORDER BY 
-	l.amount DESC NULLS LAST, 
-	s.visit_date ASC, 
-	s.source ASC,
-	s.medium ASC, 
-	s.campaign ASC
+SELECT * 
+FROM tab;
 
+WITH result AS (
+    SELECT
+        s.visitor_id,
+        s.source AS utm_source,
+        s.medium AS utm_medium,
+        s.campaign AS utm_campaign,
+        l.lead_id,
+        l.closing_reason,
+        l.status_id,
+        COALESCE(l.amount, 0) AS amount,
+        TO_CHAR(s.visit_date, 'YYYY-MM-DD HH24:MI:SS.MS') AS visit_date,
+        TO_CHAR(l.created_at, 'YYYY-MM-DD HH24:MI:SS.MS') AS created_at
+    FROM sessions AS s
+    INNER JOIN tab AS t
+        ON s.visit_date = t.last_visit AND s.visitor_id = t.visitor_id
+    LEFT JOIN leads AS l
+        ON s.visitor_id = l.visitor_id AND s.visit_date <= l.created_at
+)
+
+SELECT *
+FROM result
+ORDER BY
+    amount DESC NULLS LAST,
+    visit_date ASC,
+    utm_source ASC, 
+    utm_medium ASC, 
+    utm_campaign ASC; 
